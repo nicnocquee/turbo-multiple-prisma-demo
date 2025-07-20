76e3b4d40759295b76ce44e7d01423f39620ff22
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2025-07-20 23:37:00 +0200

    grr

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,9 @@
 import { prisma } from "@repo/database";
+import { cookies } from "next/headers";
 
 export default async function IndexPage() {
+  const cookiesList = await cookies();
+  console.log(cookiesList);
   const users = await prisma.user.findMany();
 
   return (
